96091c28996f1c38ffcf89343adcad636fc7d9c2
Richard Moore <Rmoore424@gmail.com>
2015-02-07 16:35:38 -0500

    added login/logout functionality. Added route and view for My Recipes page

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ require("./filters")(swig);
 var bodyParser = require("body-parser");
 var path = require("path");
 var passport = require("passport");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var uuid = require("uuid");
 
 var routes = require("./routes/index");
 
@@ -14,16 +17,37 @@ app.engine("html", swig.renderFile);
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "html");
 
-app.use(express.static(path.join(__dirname, "public")));
-//app.use(express.session());
+app.use(function noCachePlease(req, res, next) {
+  res.header("Cache-Control", "private, no-cache, no-store");
+  res.header("Expires", 300000);
+  res.header("Pragma", "no-cache");
+	next();
+});
+
+app.use(cookieParser());
+
+app.use(session({
+	genid: function(req) {
+		return uuid.v4();
+	},
+	secret: 'monkeypants',
+	resave: false,
+	saveUninitialized: false,
+	// cookie: {
+	// 	maxAge: 1200
+	// }
+}))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use("/", routes);
 
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.use(function(req, res, next) {
-	var err = new Error("Not Found");
+	var err = new Error("Your session has expired");
 	err.status = 404;
 	next(err);
 });
@@ -60,8 +84,14 @@ passport.serializeUser(function(user, done) {
 
 passport.deserializeUser(function(id, done) {
   User.findById(id, function (err, user) {
+  	console.log(user);
     done(err, user);
   });
 });
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) { return next(); }
+//   res.redirect('/');
+// }
 
 module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var Recipe = require("../models");
 
 var User;
 var Schema = mongoose.Schema;
@@ -8,9 +9,14 @@ var UserSchema = new Schema({
 	last_name: { type: String, required: true },
 	email: { type: String, required: true },
 	username: { type: String, required: true },
-	password: { type: String, required: true } // hash this password -this.isModified-
+	password: { type: String, required: true }, // hash this password -this.isModified-
+	user_recipes: [Recipe.Schema]
 });
 
 User = mongoose.model("User", UserSchema);
+
+UserSchema.statics.findById = function(id, cb) {
+	this.findOne({_id: id}, cb);
+}
 
 module.exports = User;
